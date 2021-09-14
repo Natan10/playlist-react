fb49ce6ec96d63f0acd32dae735b47f03f60c0d6
Natan <natan-moreira1@hotmail.com>
2021-09-14 00:19:11 -0300

    config inicial do webpack

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, '/src/main/index.tsx'),
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module:{
+    rules:[
+      {
+        test: /\.ts(x?)$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      },{
+        test: /\.scss$/,
+        use:['style-loader','css-loader','sass-loader']
+      }, 
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader",
+      },
+    ]
+  },
+  resolve: {
+    extensions: [".tsx",".ts",".js","scss"],
+  },
+  devServer:{
+    static: 'public', 
+    historyApiFallback: true,
+  }
+
+}
