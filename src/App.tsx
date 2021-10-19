7ad3f24c0c29bc4a1ad2a94888ae4b4758517512
Natan <natan-moreira1@hotmail.com>
2021-10-18 23:20:28 -0300

    refactor: change App to use styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React,{useState} from 'react';
-import Player from "./components/player";
+import styled from "styled-components"
+import {Player} from "./components";
 import out from "../assets/out.mp3";
 import baco from "../assets/baco.mp3";
 import daft from "../assets/daft.mp3";
 import artic from "../assets/artic.mp3";
 import clorine from "../assets/clorine.mp3";
-import "./style.scss";
 
 const mockSounds = [
   {
@@ -34,6 +34,91 @@ type Props = {
   soundName: string
   sound: string
 }
+
+const MainWrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: #000011;
+  padding: 40px;
+
+  h1{
+    color: white;
+    margin-bottom: 10px;
+    text-align: center;
+  }
+`
+
+const PlaylistWrapper = styled.div` 
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  border: 2px solid rgba(255,255,255,0.8);
+  border-radius: 10px;
+  padding: 40px;
+  max-width: 600px;
+  width: 100%;
+  height: 600px;
+  position: relative;
+
+  .title{
+    position: absolute;
+    top: -24px;
+    left: 24px;
+    background: #010311;
+    padding: 0 14px;
+  }
+
+  @media(max-width: 450px){
+    .title{
+      font-size: 24px;
+      top: -17px;
+      left: 24px;
+    }
+  }
+`
+
+const PlaylistSongs = styled.ul`
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  margin-bottom: 24px;   
+  list-style-type: none;
+  color: white;
+  text-align: center;
+  font-size: 24px;
+  overflow-y: scroll;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+  li{
+    cursor: pointer;
+    margin: 10px 0px;
+    font-size: 24px;
+    border-radius: 2px;
+
+    &.active {
+      background-color: #04AA6D;
+    }
+
+    &:hover{
+      background-color: rgba(255,255,255,0.4);
+      transition: background-color 0.8s;
+    }
+  }
+  @media(max-width: 450px){
+    li {
+      font-size: 20px;
+    }    
+  }
+`
+
+
 
 export default function App(){
   const [sounds] = useState<Props[]>(mockSounds);
@@ -85,12 +170,12 @@ export default function App(){
 
 
   return(
-    <div className="main">
-      <div className="playlist">
+    <MainWrapper>
+      <PlaylistWrapper>
         <h1 className="title">Playlist</h1>
-      
-        {/* list sounds */}
-        <ul className="playlist-sounds">
+
+        {/* list songs */}
+        <PlaylistSongs>
           {sounds.map((soundItem,index) => {
             return <li
                       key={index} 
@@ -99,8 +184,8 @@ export default function App(){
                       {soundItem.soundName}
                     </li>
           })}
-        </ul>
-        
+        </PlaylistSongs>
+
         {/* player */}
         <Player 
           sound={sound?.sound} 
@@ -108,7 +193,7 @@ export default function App(){
           back={backMusic}
           disabled={sound ? false : true}
         />
-      </div>
-    </div>
+      </PlaylistWrapper>
+    </MainWrapper>
   )
 }
